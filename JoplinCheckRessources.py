900d94fe20c19a8bb9c43d2df6daf7302b0e31b1
CYBERNEURONES <moi@arias-frederic.org>
2020-04-14 09:52:24 +0200

    first commit

---
+++ JoplinCheckRessources.py
@@ -0,0 +1,79 @@
+#
+# Version 2 
+# for Python 3
+# 
+#  @author : <@cyber-neurones.org>
+#   
+#   Sorry ... It's difficult for me the python :)
+#
+
+from time import gmtime, strftime
+import time
+import json
+import requests
+import os
+from tqdm import tqdm
+import hashlib
+
+strftime("%Y-%m-%d %H:%M:%S", gmtime())
+start = time.time()
+
+#Token
+ip = "127.0.0.1"
+port = "41184"
+token = "Put token here"
+nb_request = 0
+nb_file = 0
+nb_double = 0
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+url_resources = (
+    "http://"+ip+":"+port+"/resources?"
+    "token="+token
+)
+ALL_MD5 = {}
+ALL_ID = {}
+try:
+    resp = requests.get(url_resources, headers=headers)
+    nb_request += 1
+    resp.raise_for_status()
+    resp_dict = resp.json()
+    #print(resp_dict)
+    for my_resource in resp_dict:
+        my_id = my_resource.get('id')
+        my_title = my_resource.get('title')
+        my_mime = my_resource.get('mime')
+        my_ext = my_resource.get('file_extension')
+        my_type = my_resource.get('type_')
+        url_one_resources = (
+    "http://"+ip+":"+port+"/resources/"+my_resource.get('id')+"/file?"
+    "token="+token
+)
+        try:
+            resp2 = requests.get(url_one_resources, headers=headers, stream=True)
+            nb_request += 1
+            nb_file += 1
+            with open("myfile", "wb") as handle:
+                for data in tqdm(resp2.iter_content()):
+                      handle.write(data)
+            my_md5 = hashlib.md5(open('myfile','rb').read()).hexdigest()
+            #print("MD5:",my_md5)
+            if my_md5 in ALL_MD5:
+                 print("Aie")
+                 nb_double += 1
+                 ALL_ID[my_id] = my_md5
+                 my_doublon = ALL_MD5[my_md5]
+            else:
+                 ALL_MD5[my_md5] = my_id
+                 ALL_ID[my_id] = my_md5
+                 my_doublon = ''
+        except requests.exceptions.HTTPError as e:
+            print("Bad HTTP status code:", e)
+        except requests.exceptions.RequestException as e:
+            print("Network error:", e)
+        print("LIST:",my_id,",",my_title,",",my_mime,",",my_ext,",",nb_file,",",my_type,",",nb_request,",MD5,",my_md5,",SAME_AS,",my_doublon,"\n")
+except requests.exceptions.HTTPError as e:
+    print("Bad HTTP status code:", e)
+except requests.exceptions.RequestException as e:
+    print("Network error:", e)
+
+print ("Number total of request",nb_request," Number of file ",nb_file," Number of same file ",nb_double)
---
+++ README.md
@@ -0,0 +1,46 @@
+# Joplin
+#
+# ARIAS Frederic.
+#
+# Licence : OPEN BAR :)
+#
+
+JOPLIN : https://joplin.cozic.net/
+
+Python : 
+  REST API JOPLIN : https://joplin.cozic.net/api/
+
+Script 1 : DiaroToJoplin.py : Import Diaro Backup to Joplin (REST API).
+ More information : http://www.cyber-neurones.org/2019/02/diaro-app-pixel-crater-ltd-diarobackup-xml-how-to-migrate-data-to-joplin/ 
+ Test with Python 3 and Mac OS.
+
+Script 2 : GooglePlusToJoplin.py : Import Google+ Takeout to Joplin (REST API).
+ More information : http://www.cyber-neurones.org/2019/02/how-to-import-data-of-google-to-joplin/ 
+ Test with Python 3 and Mac OS.
+
+Script 3 : WordpressToJopin.py : Import Wordpress RSS to Joplin (REST API).
+ More information : http://www.cyber-neurones.org/2019/02/how-to-import-data-of-wordpress-feed-rss-to-joplin/ 
+ Test with Python 3 and Mac OS.
+
+Script 4 : JoplinToGoogleMap.py (with REST API and gmplot) : Put plot of notes (with lat and long) on Google Map.
+ More information : http://www.cyber-neurones.org/2019/02/joplin-python-gmplot-google-map-how-to-create-a-maps-with-rest-api-and-data-in-joplin/ 
+ Test with Python 3 and Mac OS.
+
+Script 5 : JoplinToStaticMap.py (with REST API and gmplot) : Put plot of notes (with lat and long)  on static map and create a PNG. 
+ More information : http://www.cyber-neurones.org/2019/02/joplin-python-static-map-how-to-create-a-maps-jpeg-with-rest-api-and-data-in-joplin/ 
+ Test with Python 3 and Mac OS. 
+
+Script 6 : JoplinToWorldCloudBody.py : (with REST API)
+ 
+
+Script 7 : JoplinToWorldCloudTitle.py : (with REST API)
+ 
+
+Script 8 : JoplinCheckRessources.py :  (with REST API)
+ 
+
+Script 9 : JoplinCleanRessource.py : (with REST API)
+ Clean ressource not used in JOPLIN.
+ More information : http://www.cyber-neurones.org/2019/03/python-how-to-clean-ressources-of-joplin-not-used/ 
+ Test with Python 3 and Mac OS.
+
